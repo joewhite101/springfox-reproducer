fff29596a680bf96b18b409d6f2ba78d4bfdc1c1
joewhite101 <j.white@entando.com>
2020-08-29 12:29:51 +0200

    add reproducer for springfox error

--- src/main/java/com/mycompany/myapp/config/apidoc/SwaggerConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/apidoc/SwaggerConfiguration.java
@@ -9,12 +9,12 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Primary
 @Profile(JHipsterConstants.SPRING_PROFILE_SWAGGER)
 @Configuration
-@EnableSwagger2WebFlux
+@EnableSwagger2
 public class SwaggerConfiguration {
 
     @Bean
